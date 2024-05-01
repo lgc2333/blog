7067d82c8553806c31b850fcb416ccc2161119b3
student_2333 <lgc2333@126.com>
2024-05-01 08:49:43 +0800

    Update main.ts

--- setup/main.ts
+++ setup/main.ts
@@ -1,9 +1,10 @@
 import { defineAppSetup } from 'valaxy'
-import VueSvgInlinePlugin from 'vue-svg-inline-plugin'
 
 import { install as installGtag } from './gtag'
 
-export default defineAppSetup((ctx) => {
+export default defineAppSetup(async (ctx) => {
   installGtag(ctx)
+
+  const VueSvgInlinePlugin = (await import('vue-svg-inline-plugin')).default
   ctx.app.use(VueSvgInlinePlugin)
 })
