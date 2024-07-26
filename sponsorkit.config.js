b03f014e7d8b810a8bfdd0bdc100aab55b9e88fa
student_2333 <lgc2333@126.com>
2024-07-26 20:34:42 +0800

    up

--- sponsorkit.config.js
+++ sponsorkit.config.js
@@ -1,19 +1,70 @@
-import { defineConfig, presets } from 'sponsorkit'
+import { readFile } from 'fs/promises'
+
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+/**
+ * @typedef {Object} DirectSponsorRawData
+ * @property {string} name
+ * @property {string} [username]
+ * @property {string} [avatar]
+ * @property {number} moneyCNY
+ * @property {string} [url]
+ */
+
+/**
+ * @returns {Promise<number>}
+ */
+async function getExchangeRate() {
+  /** @type { { rates: { USD: number }} } */
+  const data = await (
+    await fetch('https://api.fxratesapi.com/latest?base=CNY&currencies=USD')
+  ).json()
+  return data.rates.USD
+}
+
+/** @type {import('sponsorkit').Provider} */
+const directProvider = {
+  name: 'direct',
+  async fetchSponsors() {
+    /** @type {DirectSponsorRawData[]} */
+    const rawSponsors = JSON.parse(
+      await readFile('public/sponsors/direct-raw.json', { encoding: 'utf-8' }),
+    )
+    const exchangeRate = await getExchangeRate()
+
+    /** @type {import('sponsorkit').Sponsorship[]} */
+    const sponsors = rawSponsors.map((x) => ({
+      sponsor: {
+        type: 'User',
+        login: x.username ?? x.name,
+        name: x.name,
+        avatarUrl: x.avatar || '',
+        linkUrl: x.url,
+      },
+      monthlyDollars: x.moneyCNY * exchangeRate,
+      provider: this.name,
+      raw: x,
+    }))
+    return sponsors
+  },
+}
 
 export default defineConfig({
-  providers: ['afdian'],
+  providers: ['afdian', directProvider],
   formats: ['svg'],
-  outputDir: '.sponsorkit',
-  name: '../public/sponsors/afdian',
-  tiers: [
+  cacheFile: '../../.sponsorkit/.cache.json',
+  outputDir: './public/sponsors',
+  includePastSponsors: true,
+  fallbackAvatar: await readFile('public/assets/avatar-none.jpg'),
+  tiers: [{ preset: tierPresets.large }],
+  renders: [
     {
-      title: 'Sponsors',
-      preset: presets.large,
+      name: 'afdian',
+      filter: (x) => x.provider === 'afdian',
     },
     {
-      title: 'Past Sponsors',
-      monthlyDollars: -1,
-      preset: presets.large,
+      name: 'direct',
+      filter: (x) => x.provider === 'direct',
     },
   ],
 })
